3a1ea4c6f2106ef89e3b487e2d94d4b64df3207d
Gustavo Franca <gustavocandidofranca@gmail.com>
2020-06-18 10:13:53 -0300

    Arquivo js principal da aplicação front-end

---
+++ Public/app.js
@@ -0,0 +1,53 @@
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'Public',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app'
+    }
+});
+
+// Start the main app logic.
+requirejs(['controllers/Options','lib/keymap','js/nav'],
+function   (executeSelectedOption,keymap) {
+    /*Escutando o clicknos botões da calculadora*/
+    var fieldsCalculator = document.querySelectorAll('.calculator .grid .field');
+
+
+    for(const fieldCalculator of fieldsCalculator ){
+        fieldCalculator.onclick = (event) =>{   
+            const SelectedOption = event.target.textContent;
+            handleChosenOption(SelectedOption);
+        }
+    }
+    /*escutando os teclas do teclado */
+    document.addEventListener('keydown',handlekeydown);
+    /*tratando as teclas do teclado*/
+    function handlekeydown(event){
+        event = event || window.event;
+        var key = event.key;
+        if (key != null && key !== 'Unidentified') {
+        handleChosenOption(key);
+        }
+    }
+    /*chamada de função principal*/
+    function handleChosenOption(SelectedOption){
+        if(executeSelectedOption[`${keymap[SelectedOption]}`] != null)executeSelectedOption[`${keymap[SelectedOption]}`] .index();
+        return;
+    }  
+
+});
+
+
+
+
+
+
+
+
+
+
